7c2858bcdc64b4bd3ea594d53ee20792e19d352f
lion's Mac <lili5009@139.com>
2017-12-13 23:39:43 +0800

    first commit

---
+++ src/main/java/com/lion/springbootm7/SpringbootM7Application.java
@@ -0,0 +1,12 @@
+package com.lion.springbootm7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootM7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootM7Application.class, args);
+	}
+}
